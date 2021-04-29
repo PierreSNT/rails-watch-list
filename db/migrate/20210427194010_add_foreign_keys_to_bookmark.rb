151ec800bf388ea426ecc3b82f3d73a33cbae3c7
Pierre-Simon Ntiruhungwa <psntir@gmail.com>
2021-04-29 12:47:12 +0100

    finished all validations and associations

--- db/migrate/20210427194010_add_foreign_keys_to_bookmark.rb
+++
@@ -1,7 +0,0 @@
-class AddForeignKeysToBookmark < ActiveRecord::Migration[6.0]
-  def change
-    add_column :bookmarks, :add_foreign_key, :string
-    add_column :bookmarks, :, :movie,
-    add_column :bookmarks, :, :list
-  end
-end
--- db/migrate/20210427201025_add_foreign_key_to_bookmark.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeyToBookmark < ActiveRecord::Migration[6.0]
-  def change
-    add_foreign_key :bookmark, :list_id
-    add_foreign_key :bookmark, :movie_id
-  end
-end
